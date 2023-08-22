dd0648de959339bde7bd4a5f761c45554436d68e
cashartle1 <139818745+cashartle1@users.noreply.github.com>
2023-08-22 15:27:01 -0500

    add newItem function
    adds new item to list of items

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function newItem(){
+
+    //add new item to list of items:
+    let li = $('<li></li> ')
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        $('#list').append(li);
+    }
+    
+
+
+}
